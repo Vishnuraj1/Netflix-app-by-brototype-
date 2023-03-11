c0787abdc6d43051169fa33ce933fb9924626512
Vishnuraj G <vraj54208@gmail.com>
2023-03-11 12:04:11 +0530

    Some more sections added and integrated trailers onClick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
+import {Originals,Action,Comedy,Romance,Horror} from './Urls'
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
 function App() {
@@ -9,8 +10,11 @@ function App() {
     <div className='App'>
      <NavBar/>
      <Banner/>
-     <RowPost title='Netflix Originals'/>
-     <RowPost title='Action' isSmall/>
+     <RowPost Url={Originals} title='Netflix Originals'/>
+     <RowPost Url={Action} title='Action' isSmall/>
+     <RowPost Url={Comedy} title='Comedy' isSmall/>
+     <RowPost Url={Romance} title='Romance' isSmall/>
+     <RowPost Url={Horror} title='Horror' isSmall/>
     </div>
   );
 }
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,28 +1,56 @@
 import React, { useEffect, useState } from 'react'
+import YouTube from 'react-youtube'
+import { API_KEY, imageUrl } from '../../Constants/Constant'
 import './RowPost.css'
 import axios from '../../axios'
-import { API_KEY, imageUrl } from '../../Constants/Constant'
 
 function RowPost(props) {
   const [movies, setMovies] = useState([])
+const[urlId,setUrlId]=useState('')
+
   useEffect(() => {
-    axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then((response) => {
-      console.log(response.data.results[0])
+    axios.get(props.Url).then((response) => {
+      console.log(response.data.results[13])
       setMovies(response.data.results)
     }).catch(err => {
       // alert("Network Error")
     })
   }, [])
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (id) => {
+    axios.get(`/movie/${id}/videos?language=en-US&api_key=${API_KEY}`).then((response)=>{
+      // console.log(response.data)
+      if(response.data.results.length!==0){
+        setUrlId(response.data.results[0])
+      }
+      else{
+        console.log("Array is empty")
+      }
+
+    })
+  }
+
   return (
     <div className='row'>
       <h2>{props.title}</h2>
       <div className="posters">
-        {movies.map((obj) => 
+        {movies.map((obj) =>
 
-          <img className={props.isSmall ? 'smallposter':'poster'} src={`${imageUrl+obj.backdrop_path}`} />
+          <img onClick={() => handleMovie(obj.id)} className={props.isSmall ? 'smallposter' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} />
 
         )}
       </div>
+
+   {  urlId && <YouTube opts={opts} videoId={urlId.key} />  }
 
     </div>
   )
